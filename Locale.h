6850380108f34b1aa22cd694119dbb48ce232873
karamansasa49-bit <karamansasa49@gmail.com>
2025-12-18 16:19:30 +0300

    Добавлены все файлы игры в ветку sanya_game

---
+++ Locale.h
@@ -0,0 +1,41 @@
+#ifndef LOCALE_H
+#define LOCALE_H
+
+#include <string>
+#include <unordered_map>
+#include <raylib.h>
+
+enum Language {
+    LANG_RUSSIAN,
+    LANG_ENGLISH
+};
+
+class LocaleSystem {
+private:
+    std::unordered_map<std::string, std::string> russianStrings;
+    std::unordered_map<std::string, std::string> englishStrings;
+    Language currentLanguage;
+    Font russianFont;
+    
+public:
+    LocaleSystem();
+
+    void SetLanguage(Language lang);
+    Language GetCurrentLanguage() const;
+    Font GetFont() const;
+
+    // Получение строки по ключу
+    const char* GetText(const std::string& key);
+
+    // Рисование текста
+    void DrawRussianText(const char* text, int posX, int posY, int fontSize, Color color);
+    int MeasureRussianText(const char* text, int fontSize);
+};
+
+// Глобальный экземпляр
+extern LocaleSystem locale;
+
+// Макросы для удобства
+#define L(key) locale.GetText(key)
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "Game.h"
+
+int main() {
+    // Создаем и запускаем игру
+    Game game;
+    game.Run();
+    
+    return 0;
+}
