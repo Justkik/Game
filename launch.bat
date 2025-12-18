155d8c69eefb71a9817fc61db774b46c1e6116b4
karamansasa49-bit <karamansasa49@gmail.com>
2025-12-18 18:36:12 +0300

    21323213

---
+++ launch.bat
@@ -0,0 +1,11 @@
+@echo off
+echo SERGEY GAME LAUNCHER
+echo.
+echo This is a Visual Studio project.
+echo.
+echo Files:
+dir
+echo.
+echo Open SGayGame.sln in Visual Studio
+echo Press F5 to build and run
+pause
